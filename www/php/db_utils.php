c58c6a5871a083531a32250d2b6b13a53a22b6fe
Gulshan Singh <gulshan@umich.edu>
2013-07-11 22:06:52 -0700

    Added db_utils.php stub functions

---
+++ www/php/db_utils.php
@@ -0,0 +1,18 @@
+<?php
+
+function connect() {
+}
+
+function insert($con, $table, $input) {
+}
+
+function select($con, $table, $input, $request = array()) {
+}
+
+function remove($con, $table, $input) {
+}
+
+function error_check($message) {
+}
+
+?>
