ea4a860cef871cbedd9c191c45a078c83b0e3696
Ariel Weingarten <asweingarten@gmail.com>
2013-07-13 06:10:27 -0700

    Can now filter persons based on name clicked

--- www/js/views/person_collection_view.js
+++ www/js/views/person_collection_view.js
@@ -9,35 +9,90 @@ define([
 {
 	var PersonCollectionView = Backbone.View.extend(
 	{
-		tagName: 'ul',
+		tagName: 'div',
 		className: 'personCollection',
+
+		events:
+		{
+			'click .filterable-person-name': 'filterByName'
+		},
+
 
 		initialize: function()
 		{
-			_.bindAll( this, 'render' );
+			_.bindAll( this, 'render', 'collectNames', 'filterByName' );
+			this.names = [];
 			this.collection = new PersonCollection();
 			this.collection.on( 'add', this.render, this );
+			this.collection.on( 'add', this.collectNames, this );
 		},
 
 		render: function()
 		{
 
-			$( '#people-wrapper' ).empty();
+			this.$el.empty();
 
 			var data = {};
 
 			var compiled_template = _.template( Template, data );
 
-			// this.$el.append( compiled_template );
+			this.$el.append( compiled_template );
 
+			// Main Panel
 			_.each( this.collection.models, function( model )
 			{
 				var view = new PersonView( model );
-				this.$el.append( view.render() );
+				this.$('#cur-persons').append( view.render() );
 			}, this );
 
 
+			// Sidebar
+			this.$( '#person-names' ).append( _.template( "<li class='filterable-person-name' id=\"show-all-people\">Show All</li>" ) );
+			_.each( this.names, function( name )
+			{
+				this.$( '#person-names' ).append( _.template( "<li class='filterable-person-name' id=\"<%=name%>\"><%=name%></li>", { name: name } ) );
+			}, this );
+
 			$( '#people-wrapper' ).append( this.$el );
+		},
+
+		collectNames: function()
+		{
+			this.names = [];
+			_.each( this.collection.models, function( model)
+				{
+					this.names.push( model.get( 'name' ) );
+				}, this );
+			console.log( this.names );
+		},
+
+		filterByName: function( e )
+		{
+			var id = e.currentTarget.id;
+			if( id == 'show-all-people' )
+			{
+				_.each( $( '#cur-persons' ).children(), function( person )
+				{
+					$( person ).show();
+				}, this );
+				return;
+			}
+			else
+			{
+				// var people = $( '#cur-persons' ).children();
+				_.each( $( '#cur-persons' ).children(), function( person )
+				{
+					if( $( person ).attr( 'id' ) != id )
+					{
+						$( person ).hide();
+					}
+					else
+					{
+						$( person ).show();
+					}
+				}, this );
+			}
+			console.log( 'name clicked' );
 		}
 
 	});
