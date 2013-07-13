d6e0f1fd36fcc39bdac3bd02892615a024ef80fc
Ariel Weingarten <asweingarten@gmail.com>
2013-07-12 22:46:33 -0700

    Added a PersonCollection to PersonCollectionView
    Can now make real templates

--- www/js/views/person_collection_view.js
+++ www/js/views/person_collection_view.js
@@ -3,12 +3,20 @@ define([
 	'underscore',
 	'backbone',
 	'views/person_view',
+	'collections/person_collection',
 	'text!templates/person_collection.html'
-], function( $, _, Backbone, PersonView, template )
+], function( $, _, Backbone, PersonView, PersonCollection, template )
 {
 	var PersonCollectionView = Backbone.View.extend(
 	{
 		el: $( '#people-wrapper' ),
+
+		initialize: function()
+		{
+			this.collection = new PersonCollection();
+			this.collection.add( {} );
+
+		},
 
 		render: function()
 		{
