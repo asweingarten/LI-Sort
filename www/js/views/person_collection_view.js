03c50297ec12454d6550f5fc9c5e164ca6fa5921
Ariel Weingarten <asweingarten@gmail.com>
2013-07-12 21:12:47 -0700

    Added person_collection view and template
    Going to attempt rendering next

---
+++ www/js/views/person_collection_view.js
@@ -0,0 +1,23 @@
+define([
+	'jquery',
+	'underscore',
+	'backbone',
+	'views/person_view',
+	'text!templates/person_collection.html'
+], function( $, _, Backbone, PersonView, template )
+{
+	var PersonCollectionView = Backbone.view.extend(
+	{
+		el: $( '#person-collection-view-wrapper' ),
+
+		render: function()
+		{
+			var data = {};
+
+			var compiled_template = _.template( template, data );
+
+			this.$el.append( compiled_template );
+		}
+
+	});
+});
